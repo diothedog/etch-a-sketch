1d39d8d7fe1fb60b11e2752504a7ee29ed18eb24
diothedog <88989273+diothedog@users.noreply.github.com>
2024-05-30 15:18:05 -0500

    Add functionality to grid size button

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const container = document.querySelector("#container");
+const gridSizeBtn = document.querySelector("#gridSizeBtn");
+let size = 16;
 
 function createGrid(gridSize) {
     for (let i = 0; i < gridSize ** 2; i++) {
         let square = document.createElement("div");
         square.classList.add("square");
+        square.style.width = `${960 / gridSize}px`;
+        square.style.height = `${960 / gridSize}px`;
         square.addEventListener("mouseover", () => {
             square.style.backgroundColor = "black";
         });
@@ -11,5 +15,16 @@ function createGrid(gridSize) {
     }
 }
 
-createGrid(16);
+gridSizeBtn.addEventListener("click", () => {
+    newSize = prompt("How many squares per row (limit 100)");
+    if (newSize <= 100 && newSize >= 1) {
+        const squares = document.querySelectorAll(".square");
+        squares.forEach(e => e.remove());
+        createGrid(newSize);
+    }   else {
+        alert("Please enter a value between 1 and 100");
+    }
+})
+
+createGrid(size);
 
